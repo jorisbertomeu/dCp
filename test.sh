fafc9427e90dc48058bf9d83c10f236b3d7dfa92
Joris Bertomeu <joris.bertomeu@epitech.eu>
2016-05-03 11:56:34 +0200

    Verbose + download for test4 logged + ls summary

--- test.sh
+++ test.sh
@@ -33,6 +33,7 @@ checkDiff ./tests/test2/testFile ./tests/test2/testFileCopied
 #TEST 3
 echo -e "\033[33m[TEST 3/$NBR_TEST]\033[0m"
 echo -e "\033[34mDescription : Basic copy (png file)\033[0m"
+echo "Downloading file ... Please, wait"
 mkdir ./tests/test3
 wget https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/200px-Tux.svg.png -o /dev/null
 mv 200px-Tux.svg.png ./tests/test3/testImage.png
@@ -43,7 +44,10 @@ echo -e "\033[33m[TEST 4/$NBR_TEST]\033[0m"
 echo -e "\033[34mDescription : Basic copy (big file - 100MB)\033[0m"
 echo "Downloading file ... Please, wait"
 mkdir ./tests/test4
-wget http://download.thinkbroadband.com/100MB.zip -o test4_download.log
+wget http://download.thinkbroadband.com/100MB.zip -o ./tests/test4/download.log
 mv 100MB.zip ./tests/test4/testBigFile.zip
 ./dcp ./tests/test4/testBigFile.zip ./tests/test4/testBigFileCopied.zip
 checkDiff ./tests/test4/testBigFile.zip ./tests/test4/testBigFileCopied.zip
+
+#LS SUMMARY
+ls -Rrtl ./tests
