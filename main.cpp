461ff95c7f3c51833f006ec3d3a46536f6c0f526
Joris Bertomeu <joris.bertomeu@epitech.eu>
2016-05-03 12:35:37 +0200

    Percentage fixed for values > signed ing

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 void		tmpCopyCalllback(ssize_t written, ssize_t total_written, void *o, std::string to)
 {
   File		*from = reinterpret_cast<File*>(o);
-  int		percent = ((int) total_written * 100) / from->getSize();
+  unsigned int	percent = ((unsigned int) total_written * 100) / from->getSize();
 
   printf("\rCopying %d/%d bytes from %s to %s (%d%%)", total_written, from->getSize(), from->getFilename().c_str(), to.c_str(), percent);
   fflush(stdout);
