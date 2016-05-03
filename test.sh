df82b790d3b6a52d938ab15c0a45ec0a35709bb2
Joris Bertomeu <joris.bertomeu@epitech.eu>
2016-05-03 12:14:34 +0200

    beautifying test output

--- test.sh
+++ test.sh
@@ -9,29 +9,34 @@ function checkDiff() {
     if [ "$result" -eq "0" ]; then
        echo -e "\033[32m>> No difference\033[0m"
     else
-       echo -e "\033[31m>> There is a difference between both files\033[0m"
+       echo -e "\033[31m>> Files are differents\033[0m"
     fi
+    rm -fr diffRes
 }
 
 rm -fr tests
 mkdir tests
 #Compilation
-echo -e "\033[33m[COMPILATION]\033[0m"
+echo -e "\033[33m----- [COMPILATION] -----\033[0m"
 make all
+echo -e "\033[33m-------------------------\033[0m"
 #TEST 1
-echo -e "\033[33m[TEST 1/$NBR_TEST]\033[0m"
+echo -e "\033[33m----- [TEST 1/$NBR_TEST] -----\033[0m"
 echo -e "\033[34mDescription : Test Usage\033[0m"
 ./dcp
+echo -e "\033[33m----------------------\033[0m"
 #TEST 2
-echo -e "\033[33m[TEST 2/$NBR_TEST]\033[0m"
+echo -e "\033[33m----- [TEST 2/$NBR_TEST] -----\033[0m"
 echo -e "\033[34mDescription : Basic copy (little text file)\033[0m"
 mkdir ./tests/test2
 touch ./tests/test2/testFile
 echo "test" > ./tests/test2/testFile
 ./dcp ./tests/test2/testFile ./tests/test2/testFileCopied
+#echo "titi" > ./tests/test2/testFileCopied #Test checkDiff function (Expecting 
 checkDiff ./tests/test2/testFile ./tests/test2/testFileCopied
+echo -e "\033[33m----------------------\033[0m"
 #TEST 3
-echo -e "\033[33m[TEST 3/$NBR_TEST]\033[0m"
+echo -e "\033[33m----- [TEST 3/$NBR_TEST] -----\033[0m"
 echo -e "\033[34mDescription : Basic copy (png file)\033[0m"
 echo "Downloading file ... Please, wait"
 mkdir ./tests/test3
@@ -39,15 +44,20 @@ wget https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/200px-Tux
 mv 200px-Tux.svg.png ./tests/test3/testImage.png
 ./dcp ./tests/test3/testImage.png ./tests/test3/testImageCopied.png
 checkDiff ./tests/test3/testImage.png ./tests/test3/testImageCopied.png
+echo -e "\033[33m----------------------\033[0m"
 #TEST 4
-echo -e "\033[33m[TEST 4/$NBR_TEST]\033[0m"
+echo -e "\033[33m----- [TEST 4/$NBR_TEST] -----\033[0m"
 echo -e "\033[34mDescription : Basic copy (big file - 100MB)\033[0m"
-echo "Downloading file ... Please, wait"
-mkdir ./tests/test4
-wget http://download.thinkbroadband.com/100MB.zip -o ./tests/test4/download.log
-mv 100MB.zip ./tests/test4/testBigFile.zip
-./dcp ./tests/test4/testBigFile.zip ./tests/test4/testBigFileCopied.zip
-checkDiff ./tests/test4/testBigFile.zip ./tests/test4/testBigFileCopied.zip
-
+if [ "$1" == "--fast" ]; then
+    echo -e "\033[33mTest ignored (--fast indeed)\033[0m"
+else
+    echo "Downloading file ... Please, wait"
+    mkdir ./tests/test4
+    wget http://download.thinkbroadband.com/100MB.zip -o ./tests/test4/download.log
+    mv 100MB.zip ./tests/test4/testBigFile.zip
+    ./dcp ./tests/test4/testBigFile.zip ./tests/test4/testBigFileCopied.zip
+    checkDiff ./tests/test4/testBigFile.zip ./tests/test4/testBigFileCopied.zip
+fi
+echo -e "\033[33m----------------------\033[0m"
 #LS SUMMARY
 ls -Rrtl ./tests
